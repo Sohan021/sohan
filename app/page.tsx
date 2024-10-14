d0aa871de03416f756a2a58feb3373f7e4d76869
Sohan021 <sohanrahman021@gmail.com>
2024-10-14 13:00:09 +0600

    Initial

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <span className="text-4xl">Hi, I m Sohan . . .</span>
+    </div>
+  );
+}
